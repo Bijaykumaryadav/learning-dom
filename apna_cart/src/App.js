697e1bce1166eade8f52f3e43fe7a747bfc2c97c
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-05-04 22:35:14 +0530

    making cart

--- apna_cart/src/App.js
+++ apna_cart/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ProductList from './components/ProductList';
 
 function App() {
+  const productList = [
+    {
+      price: 99999,
+      name: "Iphone 10s Max",
+      quantity: 0,
+    },
+    {
+      price: 9999,
+      name: "Redmi NOte 10s Max",
+      quantity: 0,
+    },
+  ];
   return (
     <>
-    
+      <Navbar />
+      <main className="container mt-5">
+        <ProductList product={productList} />
+      </main>
+      {/* <Footer /> */}
     </>
   );
 }

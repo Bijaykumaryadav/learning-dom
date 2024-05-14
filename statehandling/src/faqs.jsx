02e411d79dc21e54416c35936a8dad27b4e9a544
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-05-14 22:07:46 +0530

    second method for the faq

--- statehandling/src/faqs.jsx
+++ statehandling/src/faqs.jsx
@@ -1,23 +1,35 @@
-import React from "react";
-// m-2 of faq 'component based'
+import React, { useState } from "react";
+import { questions } from "./Data/faqQuestion";
+
 function Faqs() {
+  let [currentId, setCurrentId] = useState(questions[0].qno);
+
+  let Items = questions.map((itemsData, i) => {
+    let itemDetails = {
+      itemsData,
+      currentId,
+      setCurrentId,
+    };
+
+    return <FaqItems itemDetails={itemDetails} key={i} />;
+  });
   return (
     <div>
       <h1>Frequently Asked Questions (FAQs)</h1>
-      <div className="faqouter"></div>
-      <FaqItems />
-      <FaqItems />
-      <FaqItems />
+      <div className="faqouter">{Items}</div>
       <div />
     </div>
   );
 }
 
-function FaqItems() {
+function FaqItems({ itemDetails }) {
+  let { itemsData, currentId, setCurrentId } = itemDetails;
   return (
     <div className="faqItems">
-      <h2>Hello</h2>
-      <p>WS</p>
+      <h2 onClick={() => setCurrentId(itemsData.qno)}>{itemsData.question}</h2>
+      <p className={currentId === itemsData.qno ? "activeAns" : ""}>
+        {itemDetails.itemsData.answer}
+      </p>
     </div>
   );
 }
